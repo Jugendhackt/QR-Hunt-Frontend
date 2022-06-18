9f0ab2583c17708dddab17e21cd25128687b3ea9
Jan Pink <jan@mbp-von-jan.intern>
2022-06-18 14:58:31 +0200

    main index page hinzugefügt

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,10 @@ import React from 'react'
 import { AppProps } from 'next/app'
 
 import '../styles/index.css'
+import { MantineProvider } from '@mantine/core'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <MantineProvider emotionOptions={{key:'mantine', prepend: false}}><Component {...pageProps} /></MantineProvider>
 }
 
 export default MyApp
