e5fbf5c8e315308e7d43ea70f53d43da47abc6ae
PixelPage <80278171+PixelPage-YT@users.noreply.github.com>
2022-06-18 12:03:47 +0200

    📑 feat: UI

--- pages/scan.tsx
+++ pages/scan.tsx
@@ -1,4 +1,6 @@
-import { Text } from "@mantine/core"
+import { Button } from "@mantine/core"
+import Image from "next/image"
+import { CameraOff } from "tabler-icons-react"
 import { NavBar } from "../components/NavBar"
 
 
@@ -6,8 +8,11 @@ const Scan = () => (
     <>
         <NavBar />
         <br></br>
-        <div className="mt-32 grid place-items-center">
-            <h1>Test</h1>
+        <div className="mt-40 grid place-items-center">
+            <img src="/assets/scanner.svg"></img>
+            <Button variant="filled" color="blue" className="mt-10">
+            <CameraOff></CameraOff>
+        </Button>
         </div>
     </>
 )
