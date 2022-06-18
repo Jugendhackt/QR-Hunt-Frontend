55ca604064342499ee8e06e1d18989a9493ac7ed
PixelPage <80278171+PixelPage-YT@users.noreply.github.com>
2022-06-18 11:51:20 +0200

    🪑 feat: Scan Page and centered div

---
+++ pages/scan.tsx
@@ -0,0 +1,15 @@
+import { Text } from "@mantine/core"
+import { NavBar } from "../components/NavBar"
+
+
+const Scan = () => (
+    <>
+        <NavBar />
+        <br></br>
+        <div className="mt-32 grid place-items-center">
+            <h1>Test</h1>
+        </div>
+    </>
+)
+
+export default Scan
