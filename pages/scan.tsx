25134d85171a72862ce64a3eba3b9cb58f65817b
PixelPage <80278171+PixelPage-YT@users.noreply.github.com>
2022-06-18 12:32:21 +0200

    📖 feat: QR-Code Reader

--- pages/scan.tsx
+++ pages/scan.tsx
@@ -2,19 +2,33 @@ import { Button } from "@mantine/core"
 import Image from "next/image"
 import { CameraOff } from "tabler-icons-react"
 import { NavBar } from "../components/NavBar"
+import { useState } from "react"
+import Html5QrcodePlugin from "../components/Html5QrcodePlugin"
 
+const Scan = () => {
 
-const Scan = () => (
-    <>
-        <NavBar />
-        <br></br>
-        <div className="mt-40 grid place-items-center">
-            <img src="/assets/scanner.svg"></img>
-            <Button variant="filled" color="blue" className="mt-10">
-            <CameraOff></CameraOff>
-        </Button>
-        </div>
-    </>
-)
+    function onNewScanResult(decodedText, decodedResult) {
+        // Handle the result here.
+        console.log(decodedText)
+    }
+
+    return (
+        <>
+            <NavBar />
+            <br></br>
+            <div className="mx-20 mt-40 grid place-items-center">
+                <Html5QrcodePlugin 
+                    fps={10}
+                    qrbox={150}
+                    disableFlip={false}
+                    qrCodeSuccessCallback={onNewScanResult}
+                />
+                <Button variant="filled" color="blue" className="mt-10">
+                    <CameraOff></CameraOff>
+                </Button>
+            </div>
+        </>
+    )
+}
 
 export default Scan
